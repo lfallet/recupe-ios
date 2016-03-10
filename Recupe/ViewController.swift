5ed2abdf83fcdb32bd9c2ac34168842849791bcd
LFA <laurent.fallet@gmail.com>
2016-03-10 23:47:54 +0000

    Initial Commit

---
+++ Recupe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Recupe
+//
+//  Created by user900991 on 10/03/2016.
+//  Copyright © 2016 Recupe.net. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
