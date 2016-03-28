11a30f7eb32cd5713dd1513b587dfc92115739f0
LFA <laurent.fallet@gmail.com>
2016-03-29 00:22:33 +0100

    Attempt to fix scrollview with constraints

--- Recupe/ViewController.swift
+++ Recupe/ViewController.swift
@@ -10,16 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: Properties
+    @IBOutlet var cityUITF: [UITextField]!
+    @IBOutlet var sendButton: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK: Actions
+    @IBAction func send(sender: UIButton) {
+        cityUITF.removeAll()
     }
-
-
 }
-
